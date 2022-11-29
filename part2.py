10d97fd2f24781dd14055ac2701b43d436b8be96
zhuy3 <zhuy3@tcd.ie>
2022-11-29 19:13:30 +0000

    11.28

--- part2.py
+++ part2.py
@@ -1,8 +1,11 @@
 
 import wave 
 import numpy as np
-#from winsound import PlaySound,SND_FILENAME
-#import matplotlib.pyplot as plt
+import unittest
+from scipy import interpolate
+from playsound import playsound#,SND_FILENAME
+import matplotlib.pyplot as plt
+
 
 def write_wav(data,file_path,sampwidth,framerate):
       # create wav file
@@ -12,7 +15,7 @@ def write_wav(data,file_path,sampwidth,framerate):
             f.setframerate(framerate)
             f.writeframes(data.tostring()) 
             f.close()
-            #PlaySound(file_path,SND_FILENAME) # paly wav file
+            playsound(file_path)#PlaySound(file_path,SND_FILENAME) # paly wav file
 
 
 
@@ -33,26 +36,26 @@ def read_wav(path):
             w = np.frombuffer(data,dtype=np.int16)
       return w,sampwidth,framerate
 
-#use median information replace noise information
 
+
+#use median information replace noise information
 def get_median(data):
       data = sorted(data)
       size = len(data)
-      if size % 2 == 0: # even
-            median = (data[size//2]+data[size//2-1])/2
-            data[0] = median
-      if size % 2 == 1: # odd
+      if size % 2 == 0: # lengh is an odd number
+            print(" filter length is not a odd number")
+      if size % 2 == 1: # lengh is an odd number
             median = data[(size-1)//2]
             data[0] = median
       return data[0]
+
 
 def cublic_replece(pos_list,data_steam,filter_len = 8):
       data_steam = list(data_steam)
       data = data_steam.copy()
       half = int((filter_len)/2)
-      if filter_len % 2 == 1:
+      if filter_len % 2 != 1:
             print(" filter length is not a even number")
-            raise
       all= len(pos_list)
       count = 0
       for pos_s in pos_list:
@@ -75,4 +78,63 @@ def cublic_replece(pos_list,data_steam,filter_len = 8):
             x.extend(x1)
             x.extend(x2)
             #print(pos_s,x,y,(start_pos,pos_s[0]),end_pos,data_steam[pos_s[-1]])
+            model = interpolate.interp1d(x,y,kind='cubic')
+            new = model(x_hat)
+            #print(new,y)
+
+#Assign the interpolation result
+            for i,pos in enumerate(pos_s):
+                  data[pos] = new[i]
+            print(int(100*count/all),'%')
+      print("Done")
+      return np.array(data,dtype=np.int16)
+
+
+def cublic_filiter(clean_wav,degraded_wav,detect_wave,out_wave):
+      clean_wav,sampwidth,framerate =  read_wav('clean_z.wav')
+      degraded_wav,_,_ =  read_wav('degraded_z.wav')
+      detect_wave,_,_ = read_wav('detectionfile_z.wav')
+
+      len(clean_wav) == len(degraded_wav) ==len(detect_wave)
+      value = max(detect_wave)
+      bk_positions = np.where(detect_wave > value-1000)[0]
+      bk_inf= [[]]
+      for i in range(len(bk_positions)-1):
+            bk_inf[-1].append(bk_positions[i])
+            if bk_positions[i+1] - bk_positions[i]>1  : 
+                  bk_inf.append([])
+            if i == len(bk_positions)-2:
+                  bk_inf[-1].append(bk_positions[i+1])  
+
+
+      output_data = cublic_replece(bk_inf,degraded_wav,filter_len = 8)
+      print(type(output_data))
+      print(type(clean_wav))
+      write_wav(clean_wav,out_wave,sampwidth,framerate)
+
+      plt.plot(range(len(output_data)),degraded_wav)
+      plt.title("degraded_wav")
+      plt.show()
+
+      plt.plot(range(len(output_data)),output_data)
+      plt.title("output wave")
+      plt.show()
+      print('MSE:',mse(clean_wav,output_data))
+      return mse(clean_wav,output_data)
+
+
+
+import unittest
+class forTestTest(unittest.TestCase):  
+      def test1(self):
+            clean_wav =  read_wav('clean_z.wav')
+            degraded_wav =  read_wav('degraded_z.wav')
+            detect_wave = read_wav('detectionfile_z.wav')
+            result = cublic_filiter(clean_wav,degraded_wav,detect_wave,'c.wav')
+            self.assertNotEqual(result,0)
+
+            print(result,'result')
+
+if __name__ == '__main__':
+      unittest.main()
             
